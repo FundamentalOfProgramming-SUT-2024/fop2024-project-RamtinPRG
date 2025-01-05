874e5172f22a7c188affa7c854a713651c39997d
Ramtin Barzkar <ramtin.barzkar.3@gmail.com>
2025-01-05 13:25:06 +0330

    Fully functional signup page

--- include/rogue.h
+++ include/rogue.h
@@ -12,6 +12,7 @@
 // _______________ DEFINES ___________________
 
 #define CREDENTIALS_MAX_LENGTH 16
+#define EMAIL_MAX_LENGTH 50
 #define PASSWORD_MIN_LENGTH 7
 #define MAX_USERS 256
 
@@ -28,6 +29,7 @@ typedef struct
 {
     char username[CREDENTIALS_MAX_LENGTH + 5];
     char password[CREDENTIALS_MAX_LENGTH + 5];
+    char email[EMAIL_MAX_LENGTH + 1];
     bool signed_in;
 } Player;
 
@@ -56,7 +58,11 @@ extern Player *player;
 bool screen_setup();
 WINDOW *init_win(int x, int y, int width, int height);
 bool handle_login();
+bool handle_signup();
 bool exists_username(char *username);
+bool validate_email(char *email);
+bool validate_password(char *password);
+bool add_user(char *username, char *password, char *email);
 
 // ______________ FUNCTIONS __________________
 
@@ -121,6 +127,58 @@ bool exists_username(char *username)
     }
     fclose(file);
     return false;
+}
+
+bool validate_email(char *email)
+{
+    char *ptr = email;
+    ptr++;
+    while (*ptr && *ptr != '@')
+        ptr++;
+    ptr++;
+    if (*ptr)
+    {
+        while (*ptr && *ptr != '.')
+            ptr++;
+        if (*ptr && *(ptr + 1))
+            return true;
+        return false;
+    }
+    return false;
+}
+
+bool validate_password(char *password)
+{
+    int upper_count = 0, lower_count = 0, digit_count = 0;
+    int len = strlen(password);
+    for (int i = 0; i < len; i++)
+    {
+        if (isupper(password[i]))
+            upper_count++;
+        if (islower(password[i]))
+            lower_count++;
+        if (isdigit(password[i]))
+            digit_count++;
+    }
+    return (upper_count && lower_count && digit_count);
+}
+
+bool add_user(char *username, char *password, char *email)
+{
+    FILE *file = fopen("usernames.txt", "a");
+    fputs(username, file);
+    fputc('\n', file);
+    fclose(file);
+    char file_name[100] = "users/";
+    strcat(file_name, username);
+    strcat(file_name, ".txt");
+    file = fopen(file_name, "w");
+    fputs(password, file);
+    fputc('\n', file);
+    fputs(email, file);
+    fputc('\n', file);
+    fclose(file);
+    return 1;
 }
 
 #endif
